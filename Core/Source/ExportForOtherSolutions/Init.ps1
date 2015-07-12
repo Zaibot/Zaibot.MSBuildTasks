9248c291b63238689506c00b85b77d80f50d1b12
Tobias de Groen <tdgroen@zaibot.net>
2015-07-12 19:07:40 +0200

    work in progress

---
+++ Core/Source/ExportForOtherSolutions/Init.ps1
@@ -0,0 +1,59 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+Import-Module (Join-Path $toolsPath "MSBuild.psm1")
+
+function Copy-MSBuildTasks($project) {
+	$solutionDir = Get-SolutionDir
+	$tasksToolsPath = (Join-Path $solutionDir ".build")
+
+	if(!(Test-Path $tasksToolsPath)) {
+		mkdir $tasksToolsPath | Out-Null
+	}
+
+	Write-Host "Copying Zaibot MSBuildTasks files to $tasksToolsPath"
+	Copy-Item "$toolsPath\Zaibot.MSBuildTasks.dll" $tasksToolsPath -Force | Out-Null
+	Copy-Item "$toolsPath\Zaibot.MSBuildTasks.targets" $tasksToolsPath -Force | Out-Null
+
+	$buildFile = Join-Path $solutionDir "Build.proj"
+	
+	#if(!(Test-Path $buildFile)) {
+	#	Write-Host "Copying Sample Build.proj to $solutionDir"
+	#	Copy-Item "$toolsPath\Build.proj" $solutionDir | Out-Null
+	#}
+
+	Write-Host "Don't forget to commit the .build folder"
+	return "$tasksToolsPath"
+}
+
+function Add-Solution-Folder($buildPath) {
+	# Get the open solution.
+	$solution = Get-Interface $dte.Solution ([EnvDTE80.Solution2])
+
+	# Create the solution folder.
+	$buildFolder = $solution.Projects | Where {$_.ProjectName -eq ".build"}
+	if (!$buildFolder) {
+		$buildFolder = $solution.AddSolutionFolder(".build")
+	}
+
+	
+	# Add files to solution folder
+	$projectItems = Get-Interface $buildFolder.ProjectItems ([EnvDTE.ProjectItems])
+
+	$targetsPath = [IO.Path]::GetFullPath( (Join-Path $buildPath "Zaibot.MSBuildTasks.targets") )
+	$projectItems.AddFromFile($targetsPath)
+
+	$dllPath = [IO.Path]::GetFullPath( (Join-Path $buildPath "Zaibot.MSBuildTasks.dll") )
+	$projectItems.AddFromFile($dllPath)
+
+	#$projPath = [IO.Path]::GetFullPath( (Join-Path $buildPath "..\Build.proj") )
+	#$projectItems.AddFromFile($projPath)
+}
+
+
+function Main 
+{
+	$taskPath = Copy-MSBuildTasks $project
+	Add-Solution-Folder $taskPath
+}
+
+Main
--- Core/StateLogic.cmd
+++ Core/StateLogic.cmd
@@ -1,2 +1,2 @@
 ::CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
-msbuild Core.sln
+msbuild Core.sln /p:MyGetApiKey=b0d122da-b891-40ab-bf41-a1448b3090d3
