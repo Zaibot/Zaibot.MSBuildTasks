cc0f43cc84441b702bdd988cf0f582203f9efe47
Tobias de Groen <tdgroen@zaibot.net>
2015-08-15 21:52:03 +0200

    feature(msbuild): initial commit nuget package targets.

---
+++ Core/Source/Zaibot.MSBuildTasks.NuGet/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// ----------------------------------------
+// 
+//   Core
+//   Copyright (c) 2015 Zaibot Programs
+//   
+//   Creation: 2015-07-12
+//     Author: Tobias de Groen
+//   Location: Arnhem, The Netherlands
+// 
+//    Website: www.zaibot.net
+//    Contact: admin@zaibot.net
+//             +31 (6) 3388 3156
+// 
+// ----------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("MSBuildTasks NuGet by Zaibot")]
+[assembly: AssemblyDescription("")]
