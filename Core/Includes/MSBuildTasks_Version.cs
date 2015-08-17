f1d5307d94f03eaac795262d3fff200e691a9112
Tobias de Groen <tdgroen@zaibot.net>
2015-08-18 00:46:00 +0200

    chore(nuget): update to 0.2.3-beta.

--- Core/Includes/MSBuildTasks_Version.cs
+++ Core/Includes/MSBuildTasks_Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 [assembly: AssemblyVersion("0.2")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
-[assembly: AssemblyInformationalVersion("0.2.1.0-pre3")]
+[assembly: AssemblyFileVersion("0.2.3.0")]
+[assembly: AssemblyInformationalVersion("0.2.3-beta feature/nuspec_build")]
