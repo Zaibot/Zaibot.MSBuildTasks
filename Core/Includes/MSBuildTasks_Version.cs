23b3c456b44ef395ea9ed98f0dc776f4d4d8b00a
Tobias de Groen <tdgroen@zaibot.net>
2015-08-30 16:14:57 +0200

    upgrade to 0.2.5-beta2

--- Core/Includes/MSBuildTasks_Version.cs
+++ Core/Includes/MSBuildTasks_Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 [assembly: AssemblyVersion("0.2")]
 [assembly: AssemblyFileVersion("0.2.5.0")]
-[assembly: AssemblyInformationalVersion("0.2.5-beta1 feature/nuspec_build")]
+[assembly: AssemblyInformationalVersion("0.2.5-beta2 feature/nuspec_build revision 7 (63e3129)")]
