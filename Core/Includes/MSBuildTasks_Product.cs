b4745d68ecf75ef6cb0ff206b93a511b2b953d63
Tobias de Groen <tdgroen@zaibot.net>
2015-09-01 01:04:25 +0200

    chore(nuget): update msbuildtasks to 0.3.0
    Signed-off-by: Tobias de Groen <tdgroen@zaibot.net>

--- Core/Includes/MSBuildTasks_Product.cs
+++ Core/Includes/MSBuildTasks_Product.cs
@@ -1,25 +1,10 @@
-// ----------------------------------------
-// 
-//   MSBuildTasks
-//   Copyright (c) 2015 Zaibot Programs
-//   
-//   Creation: 2015-08-16
-//     Author: Tobias de Groen
-//   Location: Arnhem, The Netherlands
-// 
-//    Website: www.zaibot.net
-//    Contact: admin@zaibot.net
-//             +31 (6) 3388 3156
-// 
-// ----------------------------------------
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Zaibot Programs")]
-[assembly: AssemblyProduct("MSBuildTasks")]
-[assembly: AssemblyCopyright("Copyright � Zaibot Programs 2015")]
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("Product")]
+[assembly: AssemblyCopyright("Copyright © Company 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
--- Core/Includes/MSBuildTasks_Version.cs
+++ Core/Includes/MSBuildTasks_Version.cs
@@ -1,20 +1,4 @@
-// ----------------------------------------
-// 
-//   MSBuildTasks
-//   Copyright (c) 2015 Zaibot Programs
-//   
-//   Creation: 2015-08-16
-//     Author: Tobias de Groen
-//   Location: Arnhem, The Netherlands
-// 
-//    Website: www.zaibot.net
-//    Contact: admin@zaibot.net
-//             +31 (6) 3388 3156
-// 
-// ----------------------------------------
-
 using System.Reflection;
-
-[assembly: AssemblyVersion("0.2")]
-[assembly: AssemblyFileVersion("0.2.5.0")]
-[assembly: AssemblyInformationalVersion("0.2.5-beta2 feature/nuspec_build revision 7 (63e3129)")]
+[assembly: AssemblyVersion("0.3")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyInformationalVersion("0.3.0 feature/nuspec_build")]
