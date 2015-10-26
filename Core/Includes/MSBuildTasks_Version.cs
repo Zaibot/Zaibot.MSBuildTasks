ff977b6d1699ae5187360c5685ea92f1d8492fe6
Tobias de Groen <tdgroen@zaibot.net>
2015-10-26 15:36:35 +0100

    change(nuget): update to version 0.3.0 rev 2.

--- Core/Includes/MSBuildTasks_Version.cs
+++ Core/Includes/MSBuildTasks_Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 [assembly: AssemblyVersion("0.3")]
 [assembly: AssemblyFileVersion("0.3.0.0")]
-[assembly: AssemblyInformationalVersion("0.3.0 feature/nuspec_build")]
+[assembly: AssemblyInformationalVersion("0.3.0 feature/nuspec_build revision 2 (f7410c1)")]
